ee3f38f5f8c72ebe1ce03289e165961514677ae2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-09 18:12:50 -0300

    tipo para conotação científica

--- tabelas_e_tipo_de_dados/tipo_de_dados.sql
+++ tabelas_e_tipo_de_dados/tipo_de_dados.sql
@@ -5,6 +5,10 @@ int - -2^31 até 2^31
 smallint - -2^15 até 2^15
 tinyint - 0 até 255
 
+Tipo para conotação científica
+real - -3,4^38 até -1,18^-38 e 1,18^-38 até 3,4^38
+float - -1,79^308 até -2,23^-308 e 2,23^-308 até 1,79^308 
+
 decimal (10,2) - (precisão,escala)
 numeric (10,2) - (precisão,escala)
 money (10,2) - (precisão,escala)
