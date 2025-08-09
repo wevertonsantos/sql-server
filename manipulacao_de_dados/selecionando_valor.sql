5b02d0603b099d2d4cf5122e249facb8110fee10
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-09 19:31:10 -0300

    filtrando select com where

--- manipulacao_de_dados/selecionando_valor.sql
+++ manipulacao_de_dados/selecionando_valor.sql
@@ -4,3 +4,5 @@ SELECT NOME FROM TB_CLIENTE -- Selecionando a coluna nome da tabela cliente
 
 SELECT ID,NOME,CPF FROM TB_CLIENTE -- Selecionando mais de uma coluna
 
+SELECT ID,NOME,CPF FROM TB_CLIENTE
+WHERE ID = 1 -- Selecionando apenas o ID 1 da tabela
