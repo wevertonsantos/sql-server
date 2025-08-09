ef1c68a8245a38c150de1b6377a1461d4271cf6b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-09 19:30:54 -0300

    selecionando colunas e colunas em conjunto

---
+++ manipulacao_de_dados/selecionando_valor.sql
@@ -0,0 +1,6 @@
+SELECT * FROM TB_CLIENTE -- selecionando todas as colunas da tabela cliente usando (*)
+
+SELECT NOME FROM TB_CLIENTE -- Selecionando a coluna nome da tabela cliente
+
+SELECT ID,NOME,CPF FROM TB_CLIENTE -- Selecionando mais de uma coluna
+
