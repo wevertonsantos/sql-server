700bc844a9d0c40281393bbbf56f6cf9817cc715
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-09 18:54:23 -0300

    utilizando DEFAULT

--- constraints/check_e_default.sql
+++ constraints/check_e_default.sql
@@ -11,3 +11,16 @@ CREATE TABLE TB_CLIENTE
 -- Caso é preciso adicionar o check ao campo que já existe
 ALTER TABLE TB_CLIENTE
 ADD CONSTRAINT CK_CLIENTE_IDADE CHECK(IDADE > 16)
+
+/*
+CONSTRAINT - DEFAULT - Valor padrão caso não é passado nenhum valor
+*/
+
+CREATE TABLE TB_CLIENTE
+(
+    DATA_CRIACAO DATETIME2 NOT NULL DEFAULT GETDATE() -- Colocando data daquele momento com getdate passando no default
+)
+
+-- Alterando valor para um default
+ALTER TABLE TB_CLIENTE
+ADD CONSTRAINT DF_CLIENTE_DATA_CRIACAO DEFAULT(GETDATE()) FOR DATA_CRIACAO
