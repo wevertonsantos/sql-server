734ab5004eb12cb21d262af0b054bfe364de7675
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-09 19:36:08 -0300

    atualizando mais de um valor

--- manipulacao_de_dados/atualizando_valor.sql
+++ manipulacao_de_dados/atualizando_valor.sql
@@ -2,3 +2,8 @@
 UPDATE TB_CLIENTE -- atualizando tabela cliente
 SET CPF = '22222222222' -- setando na coluna cpf o valor
 WHERE ID = 1 -- onde o id é 1
+
+-- Atualizando mais de um valor
+UPDATE TB_CLIENTE
+SET CPF = '22222222244',DATA_NASCIMENTO = '1985-06-21 18:14:22.0000000'
+WHERE ID = 1
