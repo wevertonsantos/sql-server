a42abf8532dd3aa07462b26c313b721c8d944281
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-09 17:53:39 -0300

    comando para criar tabela

---
+++ tabelas_e_tipo_de_dados/criando_tabela.sql
@@ -0,0 +1,9 @@
+-- Criando tabela
+CREATE TABLE TB_PESSOAS
+(
+    ID INT, -- tipo inteiro
+    NOME VARCHAR (50), -- tipo string com 50 caractere
+    SOBRENOME VARCHAR (50),
+    IDADE INT,
+    ALTURA DECIMAL (3,2) -- tipo decimal/float - (tamanho 3, 2 casas decimais)
+);
