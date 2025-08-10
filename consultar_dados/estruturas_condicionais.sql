8d15672e280d39d3514d42ce6bb820784f182cb4
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 12:04:24 -0300

    uso do AND no WHERE

--- consultar_dados/estruturas_condicionais.sql
+++ consultar_dados/estruturas_condicionais.sql
@@ -1,3 +1,7 @@
 -- Uso do OR
 SELECT * FROM TB_PRODUTO WHERE VALOR = 100 OR TAXA = 0.25
 -- Se uma das expressões forem verdadeiras, toda a condição where é verdadeira
+
+-- Uso do AND
+SELECT * FROM TB_PRODUTO WHERE VALOR = 100 AND TAXA 0.25
+-- Somente se todas as expressões forem verdadeiras, toda a condição where é verdadeira
