22b097516d254db30bbf588ae9f938053b9f197d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 17:47:31 -0300

    utilizando having para achar duplicidade

--- agrupamento_de_dados/having.sql
+++ agrupamento_de_dados/having.sql
@@ -5,4 +5,9 @@ Utilizando HAVING
 SELECT NumeroPedido, COUNT(*) FROM TB_DETALHE_PEDIDO
 WHERE NumeroPedido = 10249
 GROUP BY NumeroPedido
-HAVING COUNT(*) = 3 -- Irá trazer apenas os que são igual a 3
+HAVING COUNT(*) = 3 -- Irá trazer a contagem dos itens apenas os que são igual a 3
+
+-- Achar item duplicado pelo having
+SELECT NomeCompleto, COUNT(NomeCompleto) FROM TB_CLIENTE
+GROUP BY NomeCompleto
+HAVING COUNT(NomeCompleto) > 1
