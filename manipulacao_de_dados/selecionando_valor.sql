8faa34b66902020e0d26ffaf67a7477b9ca351f8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 10:05:09 -0300

    utilizando IN

--- manipulacao_de_dados/selecionando_valor.sql
+++ manipulacao_de_dados/selecionando_valor.sql
@@ -6,3 +6,11 @@ SELECT ID,NOME,CPF FROM TB_CLIENTE -- Selecionando mais de uma coluna
 
 SELECT ID,NOME,CPF FROM TB_CLIENTE
 WHERE ID = 1 -- Selecionando apenas o ID 1 da tabela
+
+UPDATE TB_CLIENTE
+SET ESTADO = 'SP'
+WHERE ID IN (1,5,6) -- Selecionando o ID que está em (1,5,6) - utilizando IN para colocar + de 1 ID
+
+UPDATE TB_CLIENTE
+SET ESTADO = 'RJ'
+WHERE ID IN (7,8,9) -- Selecionando o ID que está em (1,5,6) - utilizando IN para colocar + de 1 ID
