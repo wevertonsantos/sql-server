7368580c322080a611998663cecc28a7c9f55991
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 16:42:52 -0300

    delete com join

--- associando_tabelas/update_delete_join.sql
+++ associando_tabelas/update_delete_join.sql
@@ -9,3 +9,12 @@ JOIN TB_CATEGORIA C
 ON P.CategoriaId = C.CategoriaId
 WHERE C.Descricao = 'Condimentos'
 
+/*
+DELETE com JOIN
+*/
+
+DELETE TB_PEDIDO
+FROM TB_PEDIDO P
+JOIN TB_FUNCIONARIO F
+ON P.FuncionarioId = F.FuncionarioId
+WHERE F.Cargo = 'Vice-Presidente de Vendas'
