f90794cf3ed940cb36516a35de36fab80ae53250
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 12:29:12 -0300

    operadores relacionais

--- consultar_dados/operadores_relacionais_e_between.sql
+++ consultar_dados/operadores_relacionais_e_between.sql
@@ -2,4 +2,16 @@ SELECT * FROM TB_FUNCIONARIO
 WHERE Salario = 5000 -- Igual a 5000
 
 SELECT * FROM TB_FUNCIONARIO
-WHERE Salario <> 5000 -- Diferente de 500
+WHERE Salario <> 5000 -- Diferente de 5000
+
+SELECT * FROM TB_FUNCIONARIO
+WHERE Salario < 5000 -- Menores que 5000
+
+SELECT * FROM TB_FUNCIONARIO
+WHERE Salario <= 5000 -- Menor igual de 5000
+
+SELECT * FROM TB_FUNCIONARIO
+WHERE Salario > 5000 -- Maior de 5000
+
+SELECT * FROM TB_FUNCIONARIO
+WHERE Salario >= 5000 -- Maior igual de 5000
