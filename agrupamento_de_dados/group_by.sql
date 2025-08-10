911d5d5bdb4b9bbaef9db482a3cef1f5d625b2c2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 16:49:28 -0300

    vendo a média e a soma

--- agrupamento_de_dados/group_by.sql
+++ agrupamento_de_dados/group_by.sql
@@ -3,3 +3,9 @@ WHERE NumeroPedido = 10249
 
 SELECT MIN(PRECO) FROM TB_DETALHE_PEDIDO -- Menor preço
 WHERE NumeroPedido = 10249
+
+SELECT NumeroPedido, AVG(PRECO) FROM TB_DETALHE_PEDIDO -- AVG - Média
+GROUP BY NumeroPedido -- Agrupando pelo número do pedido
+
+SELECT NumeroPedido, SUM(PRECO) FROM TB_DETALHE_PEDIDO -- SUM - Soma
+GROUP BY NumeroPedido -- Agrupando pelo número do pedido
