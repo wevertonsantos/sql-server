0a678b35ec8ad0071feb3f4859d69de467e01343
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 12:04:37 -0300

    Diretiva NOT

--- consultar_dados/estruturas_condicionais.sql
+++ consultar_dados/estruturas_condicionais.sql
@@ -5,3 +5,10 @@ SELECT * FROM TB_PRODUTO WHERE VALOR = 100 OR TAXA = 0.25
 -- Uso do AND
 SELECT * FROM TB_PRODUTO WHERE VALOR = 100 AND TAXA 0.25
 -- Somente se todas as expressões forem verdadeiras, toda a condição where é verdadeira
+
+/*
+Diretiva NOT:
+NOT (CAMPO_1 = CAMPO_2 OR CAMPO_2 = CAMPO_3)
+
+Inverte o valor das estruturas condicionais
+*/
