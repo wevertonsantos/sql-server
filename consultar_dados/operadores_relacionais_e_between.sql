4177268cbbfbe15e9c6264998abb90c615314b2b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 12:31:19 -0300

    utilizando between

--- consultar_dados/operadores_relacionais_e_between.sql
+++ consultar_dados/operadores_relacionais_e_between.sql
@@ -1,3 +1,5 @@
+/* Operadores relacionais */
+
 SELECT * FROM TB_FUNCIONARIO
 WHERE Salario = 5000 -- Igual a 5000
 
@@ -15,3 +17,8 @@ WHERE Salario > 5000 -- Maior de 5000
 
 SELECT * FROM TB_FUNCIONARIO
 WHERE Salario >= 5000 -- Maior igual de 5000
+
+/* Between */
+
+SELECT * FROM TB_FUNCIONARIO
+WHERE Salario BETWEEN 5000 AND 9500 -- Salario está entre 5000 e 9500
