8df5a7f1c80505b8c5fe805b5119983f297fe56a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 13:34:46 -0300

    right join

--- associando_tabelas/left_e_right_join.sql
+++ associando_tabelas/left_e_right_join.sql
@@ -3,6 +3,13 @@ LEFT JOIN - trazer todos os membros da esquerda independetemente se tem ligaçã
 */
 
 SELECT C.NomeCompleto, C.Cargo, C.Contato, E.Logradouro, E.Cidade, E.Pais
-FROM TB_CLIENTE C
-LEFT JOIN TB_ENDERECO E
+FROM TB_ENDERECO C
+LEFT JOIN TB_CLIENTE E
+ON C.ClienteId = E.ClienteId
+
+/* RIGHT JOIN - Trás tudo o que tem na tb_cliente (direita) que tem ligação ou não com a tb_endereco e traz da tb_endereco o que tem ligação com tb_cliente */
+
+SELECT C.NomeCompleto, C.Cargo, C.Contato, E.Logradouro, E.Cidade, E.Pais
+FROM TB_ENDERECO C
+RIGHT JOIN TB_CLIENTE E
 ON C.ClienteId = E.ClienteId
