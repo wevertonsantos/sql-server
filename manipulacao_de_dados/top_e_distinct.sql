529f5283d28be0fc6b25828f2ea4d29387ff6295
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-10 10:27:47 -0300

    utilizando top com insert e select

--- manipulacao_de_dados/top_e_distinct.sql
+++ manipulacao_de_dados/top_e_distinct.sql
@@ -3,4 +3,10 @@ SELECT TOP 3 * FROM TB_CLIENTE -- TOP faz a função de selecionar as top 3 linh
 
 /* Selecionar itens distintos (Duplicidades são eliminadas quando é para verificar colunas específicas)*/
 SELECT DISTINCT * FROM TB_CLIENTE
-SELECT DISTINCT NOME,CPF,DATA_NASCIMENTO FROM TB_CLIENTE
+SELECT DISTINCT NOME,CPF,DATA_NASCIMENTO FROM 
+
+/* Insert com TOP - para preencher rapidamente tabelas novas */
+
+INSERT TOP (3) INTO TB_CLIENTE_SP -- Inserir na TB_CLIENTE_SP os 3 itens que estão no topo da TB_CLIENTE
+SELECT NOME, CPF, DATA_NASCIMENTO, EMAIL, ESTADO FROM TB_CLIENTE -- Selecionando as colunas
+WHERE ESTADO = 'SP' -- Onde estado na TB_CLIENTE é SP
