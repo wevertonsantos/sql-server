a0dce6388478f9f0dcf5fe1dd09f78d67cfb0979
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-11 15:23:05 -0300

    função DATENAME

--- funcoes/manipulacao_de_data.sql
+++ funcoes/manipulacao_de_data.sql
@@ -6,3 +6,9 @@ SELECT CURRENT_TIMESTAMP() -- Trás data do sistema
 
 SELECT GETDATE() -- Trás data de agora do sistema
 SELECT GETUTCDATE() -- Horário de greenwich com menor precisão
+
+SELECT DATENAME(YEAR,GETDATE()) -- Retorna um nvarchar do ano da data de hoje do sistema
+SELECT DATENAME(MONTH,GETDATE()) -- Retorna o mês 
+SELECT DATENAME(DAY,GETDATE()) -- Retorna o dia 
+SELECT DATENAME(WEEK,GETDATE()) -- Retorna a semana do mês 
+SELECT DATENAME(WEEKDAY,GETDATE()) -- Retorna o dia da semana 
