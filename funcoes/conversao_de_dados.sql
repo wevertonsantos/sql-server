0aa8964225cd1533de561eadbe48c590f65a2486
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-11 13:02:38 -0300

    CAST

---
+++ funcoes/conversao_de_dados.sql
@@ -0,0 +1,5 @@
+-- CAST - função de conversão de dados padrão da linguagem sql
+
+SELECT CAST(GETDATE() AS VARCHAR) -- Trocando para getdate() para VARCHAR
+
+-- CONVERT - função exclusiva do SQL Server
