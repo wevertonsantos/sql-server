559e6cca759cc02e038f68f3f4f5b468515eb870
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-11 15:17:57 -0300

    funções datetime

---
+++ funcoes/manipulacao_de_data.sql
@@ -0,0 +1,3 @@
+SELECT SYSDATETIME() -- Trás a data do sistema
+SELECT SYSDATETIMEOFFSET() -- Trás a data do sistema e fusos horários em relação greenwich
+SELECT SYSUTCDATETIME() -- Trás o horário de greenwich
