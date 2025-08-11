4c5804d5caa7107dbac0eb0b7e9c0fda7ea14f95
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-11 16:49:29 -0300

    ROUND()

--- funcoes/funcoes_matematicas.sql
+++ funcoes/funcoes_matematicas.sql
@@ -2,3 +2,5 @@ PRINT CEILING(4.1) -- Arredondamento para cima
 PRINT FLOOR(4.9) -- Arredondamento para baixo
 
 PRINT RAND() -- Gera um número entre 0 e 1 pseudoaleatório
+
+PRINT ROUND(123.12312312,4) -- Arredondamento para um x de número
