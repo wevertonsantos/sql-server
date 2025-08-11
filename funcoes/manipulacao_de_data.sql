ecee57fce042a0c2a7cc5d84b4a00183d495f506
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-11 15:19:09 -0300

    current_timestamp

--- funcoes/manipulacao_de_data.sql
+++ funcoes/manipulacao_de_data.sql
@@ -1,3 +1,5 @@
 SELECT SYSDATETIME() -- Trás a data do sistema
 SELECT SYSDATETIMEOFFSET() -- Trás a data do sistema e fusos horários em relação greenwich
 SELECT SYSUTCDATETIME() -- Trás o horário de greenwich
+
+SELECT CURRENT_TIMESTAMP() -- Trás data do sistema
