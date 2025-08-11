8d4608a4866d0c571c869f608d244901fd43b4bb
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-11 13:06:18 -0300

    código 108

--- funcoes/conversao_de_dados.sql
+++ funcoes/conversao_de_dados.sql
@@ -5,3 +5,4 @@ SELECT CAST(GETDATE() AS VARCHAR) -- Trocando para getdate() para VARCHAR
 -- CONVERT - função exclusiva do SQL Server
 
 SELECT CONVERT(VARCHAR,GETDATE()) -- Converter a data para varchar 
+SELECT CONVERT(VARCHAR,GETDATE(),108) -- Converter a data para varchar e pegar só a hora
